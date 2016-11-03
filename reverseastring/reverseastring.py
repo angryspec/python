1d68c5f0395f7238fd63107930ddad40a63954db
angryspec <pcschmitz@gmail.com>
2016-11-02 20:50:30 -0500

    Update reverseastring.py

--- reverseastring/reverseastring.py
+++ reverseastring/reverseastring.py
@@ -1,1 +1,11 @@
 
+'''
+Created on Oct 15, 2016
+@author: paul
+Reverse a String - Enter a string 
+and the program will reverse it 
+and print it out.
+'''
+
+word = raw_input('Please enter a word: ')
+print word[::-1]
